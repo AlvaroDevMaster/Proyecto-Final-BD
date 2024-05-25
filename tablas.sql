0dc6e776ac018f045c17989a072cfb2a8aff3647
Álvaro Maqueda Orozco <72786085+AlvaroDevMaster@users.noreply.github.com>
2024-05-25 17:13:51 +0200

    Create tablas.sql

---
+++ tablas.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE Tienda;
+USE Tienda;
+CREATE TABLE Categorías (
+    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    descripción TEXT,
+    id_categoria_padre INT,
+    FOREIGN KEY (id_categoria_padre) REFERENCES Categorías(id_categoria)
+);
+
+CREATE TABLE Proveedores (
+    id_proveedor INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    contacto VARCHAR(100)
+);
+
+CREATE TABLE Clientes (
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    dirección TEXT
+);
+
+CREATE TABLE Productos (
+    id_producto INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    descripción TEXT,
+    precio DECIMAL(10, 2) NOT NULL,
+    stock INT NOT NULL,
+    id_categoria INT,
+    id_proveedor INT,
+    FOREIGN KEY (id_categoria) REFERENCES Categorías(id_categoria),
+    FOREIGN KEY (id_proveedor) REFERENCES Proveedores(id_proveedor)
+);
+
+CREATE TABLE Pedidos (
+    id_pedido INT AUTO_INCREMENT PRIMARY KEY,
+    fecha DATE NOT NULL,
+    id_cliente INT,
+    FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente)
+);
+
+CREATE TABLE Detalles_Pedidos (
+    id_pedido INT,
+    id_producto INT,
+    cantidad INT NOT NULL,
+    precio DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (id_pedido, id_producto),
+    FOREIGN KEY (id_pedido) REFERENCES Pedidos(id_pedido),
+    FOREIGN KEY (id_producto) REFERENCES Productos(id_producto)
+);
+
+CREATE TABLE Comentarios (
+    id_comentario INT AUTO_INCREMENT,
+    id_pedido INT,
+    texto TEXT NOT NULL,
+    PRIMARY KEY (id_comentario, id_pedido),
+    FOREIGN KEY (id_pedido) REFERENCES Pedidos(id_pedido)
+);
